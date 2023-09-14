5c3e33c6e1c1a3ac951ba03d72c6bea664a11aba
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-09-14 14:57:30 +0200

    feat: add search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Search from './components/search/Search';
 
 export default function App() {
 	return (
 		<main>
 			<h1 className="header">BOOK FINDER</h1>
+			<Search />
 		</main>
 	);
 }
---
+++ src/components/search/Search.tsx
@@ -0,0 +1,8 @@
+export default function Search() {
+	return (
+		<div>
+			<input className='searchBar' type='text' placeholder='Start to type...' />
+			<button className='searchButton'>Search</button>
+		</div>
+	);
+}
