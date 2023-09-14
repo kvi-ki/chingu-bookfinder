313e38ffe1486b1f7dc082d3fc93b23966475f43
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-09-14 14:39:18 +0200

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css';
 
 export default function App() {
-  return (
-    <h1>Book Finder</h1>
-  );
+	return (
+		<main>
+			<h1 className="header">BOOK FINDER</h1>
+		</main>
+	);
 }
