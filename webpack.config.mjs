4185629eb9b80e7b9a11ef3e964d234a45786b7d
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-09-14 12:34:44 +0200

    chore: add build and dev scripts

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -8,65 +8,64 @@ import TerserPlugin from 'terser-webpack-plugin';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const Config = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    clean: true
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    port: 5001,
-    open: true,
-    hot: 'only'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.mjs', '.json']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-      },
-      {
-        test: /\.(js|ts)x?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.(png|svg|jpe?g|gif)$/,
-        use: [{ loader: 'file-loader' }]
-      }
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        clean: true
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        port: 5001,
+        open: true,
+        hot: 'only'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.mjs', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.(js|ts)x?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/,
+                use: [{ loader: 'file-loader' }]
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+            }),
+            new CssMinimizerPlugin()],
+        minimize: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Deportes Hoy',
+            filename: 'index.html',
+            template: path.join(__dirname, 'src/index.html'),
+            inject: 'body'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        })
     ]
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-        terserOptions: {
-          format: {
-            comments: false,
-          },
-        },
-      }),
-      new CssMinimizerPlugin()],
-    minimize: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Deportes Hoy',
-      filename: 'index.html',
-      template: path.join(__dirname, 'src/index.html'),
-      inject: 'body'
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-    })
-  ]
 };
 
 export default Config;
