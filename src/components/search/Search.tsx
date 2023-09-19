ca50133009b80da090b0e4ae97bd31b78a0101fc
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-09-19 17:40:15 +0200

    feat: add api call search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,14 +1,35 @@
+import { useState } from 'react';
 import './Search.css';
 
 export default function Search() {
+	const [search, setSearch] = useState('');
+
+	function handleSearch(e: any) {
+		setSearch(e.target.value);
+	}
+
+	const getBooks = async () => {
+		const books = await fetch(
+			`https://www.googleapis.com/books/v1/volumes?q=` +
+				search +
+				`&key=AIzaSyDDWe8e7HlvckQteJSFzSXx0qVNG6t7xqQ`
+		);
+		const booksList = await books.json();
+
+		console.log(booksList);
+	};
+
 	return (
 		<div className='search'>
 			<input
 				className='searchBar'
 				type='search'
 				placeholder='Start to type...'
+				onKeyDown={handleSearch}
 			/>
-			<button className='searchButton'>Search</button>
+			<button className='searchButton' onClick={getBooks}>
+				Search
+			</button>
 		</div>
 	);
 }
