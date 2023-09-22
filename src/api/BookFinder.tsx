cef971645188ee7a6e0c01cfe105c33bb699316f
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-09-22 09:24:21 +0200

    chore: use axios

--- src/api/BookFinder.tsx
+++ src/api/BookFinder.tsx
@@ -1,10 +1,10 @@
-export default async function BookFinder(search: string) {
-	const books = await fetch(
-		`https://www.googleapis.com/books/v1/volumes?q=` +
-			search +
-			`&key=AIzaSyDDWe8e7HlvckQteJSFzSXx0qVNG6t7xqQ`
-	);
-	const booksList = await books.json();
+import axios from 'axios';
 
-	console.log(booksList);
+export async function findBooks(search: string) {
+	console.log(`I am looking for: ${search}`);
+	const books = await axios.get(
+		`https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyDDWe8e7HlvckQteJSFzSXx0qVNG6t7xqQ`
+	);
+
+	return books;
 }
